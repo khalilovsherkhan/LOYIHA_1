d162057d27022c555941dc577d03cf5f293c2860
khalilovsherkhan <xalilovsherxon878@gmail.com>
2024-01-29 17:08:04 +0500

    add

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
+from django.utils.safestring import mark_safe
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category', 'name', 'content', 'sherxonjon', 'data',)
+    list_filter = ("date", "name")
+
 admin.site.register(Category)
 admin.site.register(Post)
--- core/views.py
+++ core/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
-
+from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    post = Post.objects.all()
+    categories = Category.objects.all()
+    selected_category = request.GET.get('category')
+    if selected_category:
+        post = Post.objects.filter(category__name = selected_category )
+
+    else:
+        post = Post.objects.all()
+        # return HttpResponse(f"Sherxonni so'rovi xato ")
     context = {
-        'post':post
+        'post':post,
+        'categories':categories,
+        'selected_category':selected_category
 
     }
     return render(request,'index.html', context)
